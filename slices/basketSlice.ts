1cc77388b7ae3ba78ef805e58c69ed8598bc8210
banti377 <patelbanti27@yahoo.in>
2021-11-13 23:11:57 +0530

    init commit

---
+++ slices/basketSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { RootStateOrAny } from 'react-redux';
+
+const initialState = {
+  items: [],
+};
+
+export const basketSlice = createSlice({
+  name: 'basket',
+  initialState,
+  reducers: {
+    addToBasket: (state, action) => {},
+    removeFromBasket: (state, action) => {},
+  },
+});
+
+export const { addToBasket, removeFromBasket } = basketSlice.actions;
+
+// Selectors - This is how we pull information from the Global store slice
+export const selectItems = (state: RootStateOrAny) => state.basket.items;
+
+export default basketSlice.reducer;
